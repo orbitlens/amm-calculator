96d8bea9b218a6b6de304fd3281e4eeca008b172
orbitlens <orbit.lens@gmail.com>
2021-04-11 12:37:46 +0300

    Add github source code reference

--- src/views/calculator-view.js
+++ src/views/calculator-view.js
@@ -1,19 +1,25 @@
-import React, {useState} from 'react'
+import React from 'react'
 import StateView from './state-view'
 import HistoryView from './history-view'
 import ParametersView from './parameters-view'
 import OperationTabsView from './operations/operation-tabs'
 
-
 export default function CalculatorView() {
     return <div className="container">
-        <h1>AMM simulation calculator</h1>
-        <ParametersView/>
-        <StateView/>
+        <div style={{minHeight: 'calc(100vh - 9.5em)'}}>
+            <h1>AMM simulation calculator</h1>
+            <ParametersView/>
+            <StateView/>
 
-        <OperationTabsView/>
+            <OperationTabsView/>
 
-        <hr className="space"/>
-        <HistoryView/>
+            <hr className="space"/>
+            <HistoryView/>
+        </div>
+        <div style={{textAlign: 'center'}} className="space">
+            <hr/>
+            <a href="https://github.com/orbitlens/amm-calculator" target="_blank"><span
+                className="dimmed">Source code</span></a>
+        </div>
     </div>
 }
